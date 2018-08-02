7026621fac1f9affd9d27b87ac61d4049b203114
Miguel Guaño <magol@hotmail.es>
2018-08-01 20:41:33 -0500

    subjects

--- src/app/Router.config.ts
+++ src/app/Router.config.ts
@@ -5,7 +5,7 @@ import { HomeComponent } from './componets/home/home.component';
 import { TeacherComponent } from './componets/teacher/teacher.component';
 import { SearchComponent } from './componets/search/search.component';
 import { TeacherprofileComponent } from './componets/teacher/teacherprofile/teacherprofile.component';
-import { CreateSComponent } from './componets/Subjects/creates/creates.component';
+import { CreateSComponent } from './componets/subjects/creates/creates.component';
 import { ProfileTeacherComponent } from './componets/search/profile-teacher/profile-teacher.component';
 import { CommentariesComponent } from './componets/commentaries/commentaries.component';
 import { GridTeachersComponent } from './componets/search/grid-teachers/grid-teachers.component';
